9bbf6a3f3698ac9330a67b5d9bfee27f0fe4c120
ChuTuanDuc20182429 <duc.ct182429@sis.hust.edu.vn>
2023-02-15 10:47:28 +0700

    Add high score functionality

--- SnakeGame/src/com/snake/client/gui/LoginFrame.java
+++ SnakeGame/src/com/snake/client/gui/LoginFrame.java
@@ -12,26 +12,20 @@ import java.sql.*;
 public class LoginFrame {
 
     public static void main(String[] args) {
-        // String url = "jdbc:mysql://192.168.0.116:3306/HighScore";
-        // String username = "tuan";
-        // String password = "password";
+         String url = "jdbc:mysql://localhost:3306/HighScore";
+         String username = "debian-sys-maint";
+         String password = "CNCTEDLTOWKK1fFS";
         JFrame f = new JFrame("Enter Username");
         final JTextField tf = new JTextField();
         tf.setBounds(50, 50, 150, 20);
-        JButton b = new JButton("PLay");
-        b.setBounds(50, 100, 95, 30);
-        b.addActionListener(new ActionListener() {
+        JButton bPlay = new JButton("PLay");
+        bPlay.setBounds(50, 100, 95, 30);
+        bPlay.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     f.dispose();
                     Socket socket = new Socket("localhost", 1234);
                     Client client = new Client(socket, tf.getText());
-
-                    // Class.forName("com.mysql.cj.jdbc.Driver");
-                    // Connection connection = DriverManager.getConnection(url, username, password);
-                    // Statement statement = connection.createStatement();
-                    // statement.executeUpdate("insert into HighScore values ('" + tf.getText() +
-                    // "', 0)");
 
                     // connection.close();
                     client.sendInitRequest();
@@ -41,15 +35,50 @@ public class LoginFrame {
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
-                // catch (ClassNotFoundException ex) {
-                // throw new RuntimeException(ex);
-                // }
-                // (SQLException ex) {
-                // throw new RuntimeException(ex);
-                // }
             }
         });
-        f.add(b);
+        JButton bScore = new JButton("Score");
+        bScore.setBounds(50, 150, 95, 30);
+        bScore.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection connection = DriverManager.getConnection(url, username, password);
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery("select * from HighScore order by Score desc limit 4");
+                    String[][] data = new String[4][2];
+                    int row = 0;
+                    while (resultSet.next()) {
+                        int index = 1;
+                        for (int j = 0; j < 2; j++) {
+                            data[row][j] = resultSet.getString(index);
+                            index++;
+
+                        }
+                        row++;
+                    }
+                    JFrame frame = new JFrame();
+                    frame.setTitle("Highscore");
+                    String[] columnNames = {"Name", "Score"};
+                    JTable j = new JTable(data, columnNames);
+                    j.setBounds(30, 40, 200, 300);
+                    JScrollPane sp = new JScrollPane(j);
+                    frame.add(sp);
+                    // Frame Size
+                    frame.setSize(500, 200);
+                    // Frame Visible = true
+                    frame.setVisible(true);
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        f.add(bScore);
+        f.add(bPlay);
         f.add(tf);
         f.setSize(400, 400);
         f.setLayout(null);
